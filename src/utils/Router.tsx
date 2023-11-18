3aa3bb3b6ba9d142aa1b3125e621bd310400e72b
snjngmc <ge42tes@mytum.de>
2023-11-18 17:49:12 +0100

    Add funktions to the registration box

--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import App from "../App.tsx";
 import RegsitrationBox from "../components/registration_box/RegsitrationBox.tsx";
+import {useState} from "react";
 
 /**
  * Returns the pathname of the current Base URL
@@ -16,12 +17,24 @@ const getPathname = () => {
  * Implementations of the Router.
  * @return {JSX.Element} The Router
  */
+
+interface Values {
+    region: string,
+    age: number,
+    size: number,
+    totalAmount: number,
+    availableAmount: number,
+    kWhUnit: number,
+
+}
+
 function Routing() {
+    const [value, setValue] = useState<Values | null>(null);
     return (
         <BrowserRouter basename={getPathname()}>
             <Routes>
                 <Route path="/" element={<App/>}/>
-                <Route path="/test" element={<RegsitrationBox/>}/>
+                <Route path="/test" element={<RegsitrationBox setValues={setValue}/>}/>
 
             </Routes>
         </BrowserRouter>
