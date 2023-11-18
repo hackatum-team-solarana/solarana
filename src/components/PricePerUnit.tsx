8e8d3de69e466c50e6043cebf92286537ba0676a
Tim Hils <ge47cip@mytum.de>
2023-11-18 13:20:32 +0100

    added price per unit component and some first styling

--- src/components/PricePerUnit.tsx
+++ src/components/PricePerUnit.tsx
@@ -1,8 +1,16 @@
+
 import './PricePerUnit.css'
+import { Button } from '@radix-ui/themes';
 
 
 function PricePerUnit() {
-    return (<p>Hello</p>);
+    return (
+        <div>
+            <p  style={{padding: 0, margin: 1}}>price per unit</p>
+            <h3 style={{padding: 0, margin: 1, marginBottom: 18}}>10€</h3> 
+            <Button>buy now!</Button>
+        </div>
+    );
 }
 
 export default PricePerUnit
