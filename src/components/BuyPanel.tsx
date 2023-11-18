06d534ae5b105cdad072e1a630a06fd928c53918
Tim Hils <ge47cip@mytum.de>
2023-11-18 15:12:38 +0100

    added shadow around the box and other styling related changes

--- src/components/BuyPanel.tsx
+++ src/components/BuyPanel.tsx
@@ -5,38 +5,37 @@ import "./BuyPanel.css";
 
 function BuyPanel() {
     return (
-        <div>
-            
+        <div className="wrap">
+            <h4><Text>25qm Solarpanel</Text></h4>
             <Flex gap="3" align="center">
             <div className="grid-container">
-            <div className="grid-item-above">
-            <div className="grid-container">
-      
-                <div className="grid-item"><h4><Text>25qm Solarpanel</Text></h4></div>
-                <div className="grid-item"></div>
-         
-                <div className="grid-item"><Text>provider</Text></div>
-                <div className="grid-item"><Text>Hans Mueller</Text></div>
-       
-                <div className="grid-item"><Text>region</Text></div>
-                <div className="grid-item"><Text>Bayern</Text></div>
-
-                <div className="grid-item"><Text>age in years</Text></div>
-                <div className="grid-item"><Text>0</Text></div>
-
-                <div className="grid-item"><Text>kWh/unit</Text></div>
-                <div className="grid-item"><Text>11111111!!!11</Text></div>
-
-
-                </div>
-            </div>
-            <div className="grid-item-above">
-            <PricePerUnit /> 
-            </div>
-            </div>
-             
-            
                 
+                <div className="grid-item-above">
+                    <div className="grid-container">
+      
+                        <div className="grid-item"></div>
+                        <div className="grid-item"></div>
+         
+                        <div className="grid-item"><Text>provider:</Text></div>
+                        <div className="grid-item"><Text>Hans Mueller</Text></div>
+       
+                        <div className="grid-item"><Text>region:</Text></div>
+                        <div className="grid-item"><Text>Bayern</Text></div>
+
+                        <div className="grid-item"><Text>age in years:</Text></div>
+                        <div className="grid-item"><Text>0</Text></div>
+
+                        <div className="grid-item"><Text>kWh/unit:</Text></div>
+                        <div className="grid-item"><Text>11111</Text></div>
+
+                    </div>
+                </div>
+                
+                <div className="grid-item-above">
+                    <PricePerUnit /> 
+                </div>
+            
+            </div>
             </Flex>
         </div>
     );
