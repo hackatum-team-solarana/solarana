9d120bcd7f75421ee49c6cc6fe3a2f8a854aeeff
snjngmc <ge42tes@mytum.de>
2023-11-19 06:59:00 +0100

    Fix dollar

--- src/components/price_per_unit/PricePerUnit.tsx
+++ src/components/price_per_unit/PricePerUnit.tsx
@@ -7,7 +7,7 @@ function PricePerUnit(props: {price: number}) {
     return (
         <div className='price-per-unit'>
             <p  style={{padding: 0, margin: 1}}><Text size="2">price per unit:</Text></p>
-            <h3 style={{padding: 0, margin: 1, marginBottom: 18}}><Text>{ props.price }</Text></h3> 
+            <h3 style={{padding: 0, margin: 1, marginBottom: 18}}><Text style={{fontWeight:"bold"}}>{ props.price } $</Text></h3>
             <Button className={"BuyButtonNow"}> Buy now!</Button>
         </div>
     );
