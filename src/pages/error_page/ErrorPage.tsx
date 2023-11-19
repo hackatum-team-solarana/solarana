5da686f5c718879660b95eeb1c8632b97645c109
Simon Okutan <90206664+Nomiez@users.noreply.github.com>
2023-11-19 06:59:30 +0100

    Adjust Error Page

--- src/pages/error_page/ErrorPage.tsx
+++ src/pages/error_page/ErrorPage.tsx
@@ -6,9 +6,10 @@ const ErrorPage = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <div >
-                <h1>Page not found</h1>
+        <div style={{display: "flex", height: "100vh", width: "100vw", justifyContent: "center", alignItems: "center"}}>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+                <p style={{fontWeight: "bold", fontSize: "60px", margin: 0}}>404</p>
+                <h1 style={{margin: 0}}>Page not found</h1>
                 <p>The site you were looking for, doesn't exist.</p>
                 <Button className={"BackToHomeButton1"} onClick={() => {
                     navigate("/");
