51a490c17aeb740341f858c678bc78dfe282e61e
snjngmc <ge42tes@mytum.de>
2023-11-19 07:15:23 +0100

    fix error page logout

--- src/pages/error_page/ErrorPage.tsx
+++ src/pages/error_page/ErrorPage.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@radix-ui/themes";
+import {Button} from "@radix-ui/themes";
 import {useNavigate} from "react-router-dom";
 import "./styleErrorPage.css"
+import {useWallet} from "@solana/wallet-adapter-react";
 
 const ErrorPage = () => {
     const navigate = useNavigate();
+    const {disconnect} = useWallet();
 
     return (
         <div style={{display: "flex", height: "100vh", width: "100vw", justifyContent: "center", alignItems: "center"}}>
@@ -11,9 +13,13 @@ const ErrorPage = () => {
                 <p style={{fontWeight: "bold", fontSize: "60px", margin: 0}}>404</p>
                 <h1 style={{margin: 0}}>Page not found</h1>
                 <p>The site you were looking for, doesn't exist.</p>
-                <Button className={"BackToHomeButton1"} onClick={() => {
-                    navigate("/");
-                }}>Back to home page</Button>
+                <Button className={"BackToHomeButton1"}
+                        onClick={async () => {
+                            await disconnect();
+                            localStorage.clear();
+                            sessionStorage.clear();
+                            navigate("/");
+                        }}>Back to home page</Button>
             </div>
         </div>
     )
